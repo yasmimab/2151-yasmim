1b614262b616fc60c259b85ee26182c3b6dd9716
yasmimab <yasmim.barbosa.estudante@eteot.rio.br>
2022-10-11 10:43:18 -0300

    oi

---
+++ women_gym/logout.php
@@ -0,0 +1,6 @@
+<?php
+    session_start(); //inicia a sesão
+    $_SESSION = array();  //carrega o array d sessões
+    session_destroy();   //destroi as sessões iniciadas
+    header('location: ../index.php');  //redireciona o usuário para a pagina index
+?>
---
+++ women_gym/valida.php
@@ -0,0 +1,48 @@
+<?php
+    include "conexao.php";
+
+    if(isset($_POST['login'])){
+        $login = trim($_POST['login']);
+        $senha = trim($_POST['senha']);
+        
+        //escreve a sql
+        $sql = "select * from usuarios where login = '$login' and senha = '$senha'";
+        //executa a sql
+        $testeLogin = mysqli_query($conexao,$sql);
+        $existe = mysqli_num_rows($testeLogin);
+       
+        
+        if($existe){
+            //carrega a sql em um vetor(array)
+            $dados = mysqli_fetch_array($testeLogin);
+            $senha = $dados['senha'];
+            $nome = $dados['nome'];
+            $nivel = $dados['nivel'];
+            $email = $dados['email'];
+            $foto = $dados['foto'];
+
+            if(!isset($_SESSION)){
+                session_start();
+            }
+
+            $_SESSION['nome'] = $nome;
+            $_SESSION['login'] = $login;
+            $_SESSION['email'] = $email;
+            $_SESSION['nivel'] = $nivel;
+            $_SESSION['foto'] = $foto;
+
+
+            if($nivel == "adm"){
+                header('location: adm.php');
+            }
+            else{
+                header('location: ../usuarios/perfil.php');
+            }
+        }
+        else{
+            echo "Usuário ou senha inválidos.";
+        }
+    }
+    
+
+?>
